97db951ddfc1949064e18dfee14635230131f5b9
Almog <almoglevi1008@gmail.com>
2022-05-02 12:37:20 +0300

    Code fixed according to the comment

--- section1.py
+++ section1.py
@@ -1,39 +1,38 @@
 import functools
+from typing import Callable, Any
 
 
-def type_check(correct_type):
+def type_check(correct_type: type) -> Callable:
     """
-    The function gets a type that need to check if it's correct type and call to decorator_type_check function.
+    The function is a decorator factory. The function returns decorators that decorate functions.
     :param correct_type: The wanted type.
     :return: The answer of decorator_type_check function.
     """
-    def decorator_type_check(func):
+    def decorator_type_check(func: Callable) -> Callable:
         """
         The function gets function(times2) and call to inner function.
         :param func: The function that get.
         :return: The answer of inner function.
         """
         @functools.wraps(func)
-        def inner(variable):
+        def inner(variable: Any) -> Any:
             """
             The function gets a variable and checks if its type is the same as the wanted type(correct_type),
             Yes - calls to func(in this case - times2), No - throw Exception.
             :param variable: The checked variable.
             :return: The answer of func function.
             """
-            try:
-                if not isinstance(variable, correct_type):
-                    raise Exception("Incorrect type!")
+            if isinstance(variable, correct_type):
                 return func(variable)
-            except Exception as error:
-                print(error)
+            else:
+                raise ValueError("Incorrect type!")
 
         return inner
     return decorator_type_check
 
 
 @type_check(int)
-def times2(number):
+def times2(number: int) -> int:
     """
     The function gets a number and first, calls to decorator function - type_check.
     :param number: The number that the function gets.
@@ -43,4 +42,8 @@ def times2(number):
 
 
 if __name__ == "__main__":
-    print(times2(4))
+    try:
+        print(times2(4))
+        print(times2("4"))
+    except ValueError as error:
+        print(error)
