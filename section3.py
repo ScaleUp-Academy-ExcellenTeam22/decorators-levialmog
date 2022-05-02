fa430c1ef8d2e409eb8c3aa44b6962358c9dfcc7
Almog <almoglevi1008@gmail.com>
2022-05-02 13:01:47 +0300

    Code fixed according to the comment

--- section3.py
+++ section3.py
@@ -1,25 +1,28 @@
+from typing import Callable, Any
+
 import decorator
 
 
 @decorator.decorator
-def change_functionality(func,  *args, **kwargs):
+def change_functionality(func: Callable,  *args: Any, **kwargs: Any) -> int:
     """
-    The function gets function(times2) and return the string "Surprise!".
-    :param func:
-    :param _: we will not use this variable.
-    :return: The string "Surprise!".
+    The function gets function(times2) and return the result of times2 function.
+    :param func: The wrapper function for the function that was got (times2).
+    :param args: The parameter that function gets.
+    :param kwargs: The parameter that function gets.
+    :return: The value that the function times2 returned.
     """
-    return "Surprise!"
+    return func(*args, **kwargs)
 
 
 @change_functionality
-def times2(number):
+def times2(number: int) -> int:
     """
     The function gets a number and first, calls to decorator function - type_check.
     :param number: The number that the function gets.
     :return: The number * 2
     """
-    print(number)
+    return number * 2
 
 
 if __name__ == "__main__":
